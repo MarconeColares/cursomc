adfc25fbdddd32d24ccde5b18d4f414ef142d9ba
Marcone Colares <marcone.colares@gmail.com>
2021-06-08 22:19:01 -0300

    EndPoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/marconecolares/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.marconecolares.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marconecolares.cursomc.domain.Pedido;
+import com.marconecolares.cursomc.repositories.PedidoRepository;
+import com.marconecolares.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		 Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!" + id + 
+				" " + Pedido.class.getName()));
+		}
+	
+}
